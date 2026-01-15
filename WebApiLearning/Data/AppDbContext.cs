b88d15e6cf8633ac3b035ea85c3c0508c6573aa8
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2026-01-16 01:21:35 +0900

    テーブルの追加

--- WebApiLearning/Data/AppDbContext.cs
+++ WebApiLearning/Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
-
+    public DbSet<FaceLog> FaceLogs {get; set;}
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
@@ -14,5 +14,21 @@ public class User
     public int Id { get; set; }
     public string Name { get; set; } = "";
     public int Age { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
+
+public class FaceLog
+{
+    public int Id {get; set;}
+    public DateTime BirthDate {get; set;}
+
+    public bool Gender {get; set;}
+
+    public string Job {get; set;} = "";
+
+    public int EarlySociable {get; set;}
+    public int MidSociable {get; set;}
+    public int LateSociable {get; set;}
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
---
+++ WebApiLearning/Migrations/20260115162020_AddFaceLog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiLearning.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFaceLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FaceLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BirthDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Gender = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Job = table.Column<string>(type: "TEXT", nullable: false),
+                    EarlySociable = table.Column<int>(type: "INTEGER", nullable: false),
+                    MidSociable = table.Column<int>(type: "INTEGER", nullable: false),
+                    LateSociable = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FaceLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FaceLogs");
+        }
+    }
+}
