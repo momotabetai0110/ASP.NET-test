6a62fc7a4e8a619cb832b321f75b25cb56c44367
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2026-01-25 21:13:45 +0900

    Testsテーブルを作成

--- WebApiLearning/Data/AppDbContext.cs
+++ WebApiLearning/Data/AppDbContext.cs
@@ -9,6 +9,7 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+     public DbSet<Test> Tests { get; set; }
 }
 
 public class User
---
+++ WebApiLearning/Model/test.cs
@@ -0,0 +1,5 @@
+public class Test
+{
+    public int Id { get; set; }   // 主キー（PK）
+    public string Name { get; set; }
+}
